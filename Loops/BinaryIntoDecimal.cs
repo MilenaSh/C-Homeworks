5f57f4e91d1f715a70d2a223bf126a5d3b58424f
Milena <shishkova.milena@gmail.com>
2016-11-09 20:41:39 +0200

    folder Loops

---
+++ Loops/BinaryIntoDecimal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class BinaryIntoDecimal
+{
+    static void Main()
+    {
+        string number = Console.ReadLine();
+
+        char[] charArray = number.ToCharArray();
+        long sum = 0;
+
+        for (int i = charArray.Length - 1; i >= 0; i--)
+        {
+            if (charArray[charArray.Length - i - 1] == '1')
+            {
+                long result = (long)Math.Pow(2, i);
+                sum += result;
+            }
+        }
+
+        Console.WriteLine(sum);
+
+    }
+}
+
---
+++ Loops/DecimalIntoHex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class DecimalIntoHex
+{
+    static void Main()
+    {
+        long number = long.Parse(Console.ReadLine());
+        long remainder = 0;
+
+        string result = string.Empty;
+
+        while (number > 0)
+        {
+            remainder = number % 16;
+
+            if (remainder == 10)
+            {
+                remainder = "A";
+            }
+            result = remainder.ToString();
+            number = number / 16;
+
+        }
+        int a = 188;
+        int b = 16;
+
+        int result = a % 16;
+        Console.WriteLine(result);
+    }
+}
+
+
