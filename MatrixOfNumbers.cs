0156ed06a4e32fbe73b069197d724c383fa20c48
MilenaSh <shishkova.milena@gmail.com>
2016-11-09 20:22:16 +0200

    Add files via upload

---
+++ MatrixOfNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class MatrixOfNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                Console.Write(i + j + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
